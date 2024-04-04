8549ea5bd5404c5095b1c1fac6ff29b752c52ca6
VolpeDelDeserto <93005640+VolpeDelDeserto@users.noreply.github.com>
2024-04-04 16:27:44 +0800

    Update print_32767plus_vertices_mesh.py
    Added a script to deselect all the meshes with less than 32767 vertices from my selection

--- print_32767plus_vertices_mesh.py
+++ print_32767plus_vertices_mesh.py
@@ -13,3 +13,20 @@ print_selected_meshes_over_limit()
 
 #This script iterates over all selected objects in the scene and checks if the object type is a mesh.
 # If it is, it counts the number of vertices and prints out the count along with the mesh's name if the number of vertices exceeds 32767.
+
+import bpy
+
+# Get selected objects
+selected_objects = bpy.context.selected_objects
+
+# Loop through selected objects
+for obj in selected_objects:
+    # Check if object is a mesh
+    if obj.type == 'MESH':
+        # Check if vertices count is less than 32767
+        if len(obj.data.vertices) < 32767:
+            # Deselect the object
+            obj.select_set(False)
+
+
+#It will deselect any selected mesh objects with fewer than 32767 vertices.
